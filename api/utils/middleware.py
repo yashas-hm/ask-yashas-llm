6d8d63ff8c89ab4d6efd5d0cb934f4099a07ee06
Yashas H Majmudar <yashashm.dev@gmail.com>
2025-04-06 11:49:50 -0400

    feat: bypass default path

--- api/utils/middleware.py
+++ api/utils/middleware.py
@@ -3,10 +3,12 @@ from starlette.responses import JSONResponse
 
 class CORSMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request, call_next):
+        if request.url.path == '/':
+            return await call_next(request)
+        
         allowed_origin = "https://yashashm.dev"
         origin = request.headers.get("Origin")
         if origin != allowed_origin:
             return JSONResponse({"detail": "Forbidden"}, status_code=403)
 
-        response = await call_next(request)
-        return response
+        return await call_next(request)
