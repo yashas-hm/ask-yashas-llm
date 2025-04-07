c87e270a3911b395f42ca1e2acc535dcc299319e
Yashas H Majmudar <yashashm.dev@gmail.com>
2025-04-06 20:20:27 -0400

    fix: CORS middleware

--- api/utils/middleware.py
+++ api/utils/middleware.py
@@ -3,12 +3,16 @@ from starlette.responses import JSONResponse
 
 class CORSMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request, call_next):
-        if request.url.path == '/':
+        if request.url.path == "/":
             return await call_next(request)
         
-        allowed_origin = "https://yashashm.dev"
+        allowed_origin = [
+            "https://yashashm.dev",
+            "https://chat.yashashm.dev", 
+            "https://ask-yashas-llm.onrender.com-"
+        ]
         origin = request.headers.get("Origin")
-        if origin != allowed_origin:
+        if origin in allowed_origin:
             return JSONResponse({"detail": "Forbidden"}, status_code=403)
 
         return await call_next(request)
